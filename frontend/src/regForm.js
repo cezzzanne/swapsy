7153e28a33a427c80c4e319e69c85aa0e606d486
eliasaguirre <eliasaguirrea97@gmail.com>
2019-01-26 17:15:07 -0800

    fixed

--- frontend/src/regForm.js
+++ frontend/src/regForm.js
@@ -5,9 +5,9 @@ class RegForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: 'Name',
-            email: 'Email',
-            password: 'Password'
+            name: '',
+            email: '',
+            password: ''
         };
 
         this.handleNameChange = this.handleNameChange.bind(this);
@@ -34,50 +34,28 @@ class RegForm extends React.Component {
     render() {
         return (
             <div>
-            <div className="field">
             <form>
-                    <input className="input is-medium is-rounded" type="text" name='name' placeholder='name' onChange={this.handleNameChange} />
+                <label>
+                    Name:
+                    <input type="text" name='name' onChange={this.handleNameChange} />
+                </label>
             </form>
-                </div>
-                <div className="field">
             <form>
-                    <input className="input is-medium is-rounded" type="text" name='email' placeholder='email' onChange={this.handleEmailChange} />
+                <label>
+                    Email:
+                    <input type="text" name='email' onChange={this.handleEmailChange} />
+                </label>
             </form>
-                </div>
-                <div className="field">
             <form>
-<<<<<<< HEAD
-<<<<<<< HEAD
                 <label>
                     Password:
                     <input type="password" name='password' onChange={this.handlePasswordChange} />
                 </label>
-=======
-                    <input className="input is-medium is-rounded" type="text" name='password' placeholder='password' onChange={this.handlePasswordChange} />
->>>>>>> a504c07f82e1f2f7f970ef2602a9dd5666e047c8
-=======
-                    <input className="input is-medium is-rounded" type="text" name='password' placeholder='password' onChange={this.handlePasswordChange} />
->>>>>>> a504c07f82e1f2f7f970ef2602a9dd5666e047c8
             </form>
-                </div>
             <form onSubmit={this.handleSubmit}>
                 <input type="submit" value="Submit" />
             </form>
             </div>
-    // <div>
-    //     <Field>
-    //     <Input className="input is-medium is-rounded" type="text" name='name' placeholder='name' onChange={this.handleNameChange} />
-    // </Field>
-    // <Field>
-    //     <Input className="input is-medium is-rounded" type="text" name='email' placeholder='email' onChange={this.handleEmailChange} />
-    // </Field>
-    // <Field>
-    //     <Input className="input is-medium is-rounded" type="text" name='password' placeholder='password' onChange={this.handlePasswordChange} />
-    // </Field>
-    //
-    // <form onSubmit={this.handleSubmit}>
-    //         <input type="submit" value="Submit" />
-    //         </form>
 
         );
     }
