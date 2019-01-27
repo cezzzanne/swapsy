283e1bb3559ea6d2b372dc4786ed59afac9d9e7a
Brandon Forys <bforys@gmail.com>
2019-01-26 16:01:20 -0800

    Added registration form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import RegForm from './regForm.js';
 import './App.css';
 
 class App extends Component {
@@ -7,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <RegForm></RegForm>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
